0edde5743e477b2bd98f4a55679bb45de507c404
bahaeomid <bahae.omidmisc@gmail.com>
2014-04-28 21:05:32 -0600

    Commiting assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Write two functions to return cached value for inverse of a matrix, if calculated already, or
+##calculate the inverse if the inverse was not calculated.
 
-## Write a short comment describing this function
+## Write a function that takes a matrix as an input and returns a list of 4 functions to read the value of the input matrix, store the inverse of
+## the matrix, set a new value to the input matrix, and read the value of the stored inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  inv <- NULL
+  
+  readinput <- function () x
+  setnewinput <- function(y){x <<-y ; inv <<- NULL }
+  storeinverse <- function(inverse) inv <<- inverse  
+  readinverse <- function() inv
+  list(readinput=readinput,setnewinput=setnewinput,storeinverse=storeinverse,readinverse=readinverse)
+  
 
 }
 
 
-## Write a short comment describing this function
+## Read the cached inverse matrix, if caclulated already, or else calculate the inverse from scratch
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$readinverse()
+  if(!is.null(inv)){message("getting cached data");return(inv)}
+  newinv <- solve(x$readinput())
+  x$storeinverse(newinv)
+  newinv
+  
+  
 }
